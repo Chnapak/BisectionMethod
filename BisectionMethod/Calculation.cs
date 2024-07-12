260315ea6b87a911d1f3d463ae22fbbc47788429
Chnapak <matej.hladecek@gmail.com>
2024-07-12 04:16:04 +0200

    Created repository, added comments and added an iteration version.

---
+++ BisectionMethod/Calculation.cs
@@ -0,0 +1,70 @@
+﻿namespace BinarySearch
+{
+    internal class Calculation
+    {
+        // This program uses the Bisection method to find values of square roots of mainly irational numbers.
+        // There are two methods iteration and recursion. Iteration is better because it can't crash easily.
+        // It can find the square root of 2 in matter of twenty miliseconds.
+
+
+        // Iteration
+        public double BisectionLoop(double irregular)
+        {
+            double lower = 0;
+            double upper = int.MaxValue;
+
+            while(true)
+            {
+                
+                Console.WriteLine((upper + lower) / 2);
+
+                // If the average of the bounds is an overestimate, move upper to the average.
+                if (Math.Pow((upper + lower) / 2, 2) > irregular)
+                {
+                    // If the difference is unnoticable, then return the average.
+                    if ((upper + lower) / 2 == lower)
+                    {
+                        return (upper + lower) / 2;
+                    }
+
+                    upper = (upper + lower) / 2;
+                }
+                // If the average of the bounds is an underestimate, move lower to the average.
+                else if ((Math.Pow(upper + lower, 2)) / 4 < irregular)
+                {
+                    // If the difference is unnoticable, then return the average.
+                    if ((upper + lower) / 2 == upper)
+                    {
+                        return (upper + lower) / 2;
+                    }
+
+                    lower = (upper + lower) / 2;
+                }
+                // If the average is on point, then it is the square root.
+                else
+                {
+                    Console.WriteLine((upper + lower) / 2);
+                    return (upper + lower) / 2;
+                }
+            }
+        }
+        public double BisectionIteration(double irregular, double lower, double upper){
+            
+
+            // Similar logic and struture as the iteration function.
+            Console.WriteLine((upper + lower) / 2);
+
+            if (Math.Pow((upper+lower)/2, 2) > irregular)
+            {
+                return BisectionIteration(irregular, lower, (upper + lower) / 2);
+            }
+            else if ((Math.Pow(upper + lower, 2)) / 4 < irregular){
+                return BisectionIteration(irregular, (upper + lower) / 2, upper);
+            }
+            else
+            {
+                return (upper + lower) / 2;
+            }
+        }
+    }
+}
---
+++ BisectionMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Calculation();
+            app.BisectionLoop(3);
+            app.BisectionIteration(3, 0, int.MaxValue);
+        }
+    }
+}
